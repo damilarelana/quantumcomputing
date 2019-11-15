0780f6e95bcb7c19de995d71da537729eaffcb97
Damilare Lana <damilarelana@gmail.com>
2019-11-15 22:08:33 +0100

    IBM QE login details to the environment. Added getenv to extract this login details at runtime

--- projectQ/quantumcomputingFirstPrinciples/quantum_random_numbers_ibm.py
+++ projectQ/quantumcomputingFirstPrinciples/quantum_random_numbers_ibm.py
@@ -1,9 +1,15 @@
 from projectq import MainEngine  # import the main compiler engine
 from projectq.ops import H, Measure  # import the operations we want to perform (Hadamard and measurement)
 from projectq.backends import IBMBackend
+from os import getenv
 import projectq.setups.ibm
 
-eng = MainEngine(IBMBackend(),
+# extract IBM Quantum Experience login details from environ 
+ibmqe_user = getenv('IBMQE_USER')
+ibmqe_password = getenv('IBMQE_PASSWORD')
+
+# login to IBM and instantiate the quantumEngine object
+eng = MainEngine(IBMBackend(user=ibmqe_user, password=ibmqe_password),
                  engine_list=projectq.setups.ibm.get_engine_list())
 qubit = eng.allocate_qubit()  # allocate a quantum register with 1 qubit
 
